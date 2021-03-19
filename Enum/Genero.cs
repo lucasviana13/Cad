348381dbac7a4bd6140e39d846a6882f864e6846
Lucas Silva Viana <80357765+lucasviana13@users.noreply.github.com>
2021-03-19 10:18:47 -0300

    Add files via upload

---
+++ Enum/Genero.cs
@@ -0,0 +1,20 @@
+namespace Catalogo
+{
+    public enum Genero 
+    {
+        Acao = 1,
+        Animacao = 2,
+        Aventura = 3,
+        Comedia = 4,
+        Drama = 5,
+        Familia = 6,
+        Ficcao = 7,
+        Herois = 8,
+        Magia = 9,
+        Romance = 10,
+        Suspense = 11,
+        Terror = 12
+        
+
+    }
+}
